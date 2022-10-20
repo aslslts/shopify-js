f5a2cb6df14de3245deb2ec78845c9109a5cc1d2
Aslı Sulutaş <cemree.1402@gmail.com>
2022-10-20 22:37:55 +0300

    Navbar and card added

---
+++ src/main.js
@@ -0,0 +1,71 @@
+let shop = document.getElementById("shop");
+
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
+
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+        let {id, name, price, desc, img } = x;
+      return `
+      <div id="product-id-${id}" class="item">
+        <img width="220" src="${img}" alt="">
+        <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+               <h2>${price}</h2> 
+               <div class="buttons">
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                <div id="${id}" class="quantity">0</div>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+               </div>
+            </div>
+        </div>
+      </div>
+      `;
+    })
+    .join(""));
+};
+
+generateShop();
+
+let increment =() => {
+    console.log("increment");
+
+};
+let decrement =() => {
+    console.log("decrement");
+};
+let update =() => {
+    
+};
